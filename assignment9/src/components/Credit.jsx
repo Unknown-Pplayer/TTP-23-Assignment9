c2a5481b6561e28823bd727cb136b3d403b7deae
Unknown-Pplayer <xwang2230@gmail.com>
2023-06-21 09:57:33 -0400

    Finish

--- assignment9/src/components/Credit.jsx
+++ assignment9/src/components/Credit.jsx
@@ -1,32 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
+function Debit(props) {
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
+  const [transactions, setTransactions] = useState([]);
 
-function Credit(props) {
-	const [amount, setAmount] = useState();
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		const updatedBalance = props.accBalance + parseFloat(amount);
-		console.log(updatedBalance);
-		props.setAccBalance(updatedBalance);
-	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const transaction = {
+      amount: parseFloat(amount),
+      description,
+      date: new Date().toLocaleString(),
+    };
+    const updatedBalance = props.accBalance + parseFloat(amount);
+    props.setAccBalance(updatedBalance);
+    setTransactions([...transactions, transaction]);
+    setAmount("");
+    setDescription("");
+  };
 
-	return (
+  return (
     <div>
-		<h1>Credit Page</h1>
-		<p>Your Account Balance: {props.accBalance}</p>
-			<form onSubmit={handleSubmit}>
-				<label>
-					Amount: 
-					<input type="number" value={amount} onChange={(e)=> setAmount(e.target.value)}/>
-				</label>
-				<label>
-					Description: 
-					<input type="text" />
-				</label>
-				<input type="submit" />
-			</form>
+      <h1>Debit Page</h1>
+      <p>Your Account Balance: {props.accBalance}</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Amount:
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        </label>
+        <label>
+          Description:
+          <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+        </label>
+        <input type="submit" />
+      </form>
+      <h2>Transactions:</h2>
+      <ul>
+        {transactions.map((transaction, index) => (
+          <li key={index}>
+            <span>Amount: {transaction.amount}</span> &emsp;
+            <span> Description: {transaction.description}</span> &emsp;
+            <span> Date: {transaction.date}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default Credit;
+export default Debit;
--- assignment9/src/components/Debit.jsx
+++ assignment9/src/components/Debit.jsx
@@ -1,29 +1,49 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function Debit(props) {
-	const [amount, setAmount] = useState();
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		const updatedBalance = props.accBalance - parseFloat(amount);
-		console.log(updatedBalance);
-		props.setAccBalance(updatedBalance);
-	};
-	
-	return (
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
+  const [transactions, setTransactions] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const transaction = {
+      amount: parseFloat(amount),
+      description,
+      date: new Date().toLocaleString(),
+    };
+    const updatedBalance = props.accBalance - parseFloat(amount);
+    props.setAccBalance(updatedBalance);
+    setTransactions([...transactions, transaction]);
+    setAmount("");
+    setDescription("");
+  };
+
+  return (
     <div>
-		<h1>Debit Page</h1>
-		<p>Your Account Balance: {props.accBalance}</p>
-			<form onSubmit={handleSubmit}>
-				<label>
-					Amount: 
-					<input type="number" value={amount} onChange={(e)=> setAmount(e.target.value)}/>
-				</label>
-				<label>
-					Description: 
-					<input type="text" />
-				</label>
-				<input type="submit" />
-			</form>
+      <h1>Debit Page</h1>
+      <p>Your Account Balance: {props.accBalance}</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Amount:
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        </label>
+        <label>
+          Description:
+          <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+        </label>
+        <input type="submit" />
+      </form>
+      <h2>Transactions:</h2>
+      <ul>
+        {transactions.map((transaction, index) => (
+          <li key={index}>
+            <span>Amount: {transaction.amount}</span> &emsp;
+            <span> Description: {transaction.description}</span> &emsp;
+            <span> Date: {transaction.date}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
